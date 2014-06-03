e7bff93f30d7b024ec68483de97662ecb4d6f3e2
hanijarrouj <hanijarrouj@hotmail.com>
2014-06-03 23:08:57 +0400

    hani

---
+++ app/controllers/Application.java
@@ -0,0 +1,61 @@
+package controllers;
+
+
+import play.*;
+import play.data.DynamicForm;
+import play.data.Form;
+import play.mvc.*;
+import views.html.*;
+
+public class Application extends Controller {
+
+    public static Result index() {
+    	DynamicForm form = Form.form().bindFromRequest();
+    	String txt_a = form.data().get("txt_a");
+    	String txt_b = form.data().get("txt_b");
+    	String txt_c = form.data().get("txt_c");
+    	Double a =0.0, b = 0.0, c = 0.0;
+    	if (txt_a != null )
+    	{
+    		a = Double.parseDouble(txt_a);
+    	}
+    	
+    	if (txt_b != null )
+    	{
+    		b = Double.parseDouble(txt_b);
+    	}
+    	
+    	if (txt_c != null )
+    	{
+    		c = Double.parseDouble(txt_c);
+    	}
+		
+		Double D = b*b - 4*a*c;
+		
+		String result = "";
+		if(D > 0)
+		{
+			Double x1 = (-b + Math.sqrt(D) ) / 2 * a ;
+			Double x2 = (-b - Math.sqrt(D) ) / 2 * a ;
+			
+		result = "X1 = " + x1 + " , X2 = " + x2;
+			
+			
+		}
+		else if (D==0)
+		{
+			Double x1 = (-b ) / 2 * a ;
+			result = "X = " + x1 + " , X = " + x1;
+		}
+		
+		else if(D <  0)
+		{
+			result = "imposible";
+		}
+
+		
+        return ok(index.render(result));
+    }
+
+}
+
---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "hani1"
+
+version := "1.0-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+  javaJdbc,
+  javaEbean,
+  cache
+)     
+
+play.Project.playJavaSettings
